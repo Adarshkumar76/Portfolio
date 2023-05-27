3548a9d89c22433ac51651f92baf0d307eea2fcd
Adarsh <adarshku7643@gmail.com>
2023-05-27 20:35:44 +0530

    Footer modified

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import './Footer.css'
 
 function Footer() {
+
   return (
     <footer>
       <div className="rights">
-        Copyright &#169; AdarshPortfolio.com | All rights reserved
+        Copyright &#169; 2023 - {(new Date().getFullYear())} AdarshPortfolio | All rights reserved
       </div>
     </footer>
   )
