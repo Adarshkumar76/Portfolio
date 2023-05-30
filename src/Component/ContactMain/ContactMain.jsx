0293a54ac142f0769aeb3f12009817b1b1c8fad8
Adarsh <adarshku7643@gmail.com>
2023-05-30 07:41:22 +0530

    Contact page Change

--- src/Component/ContactMain/ContactMain.jsx
+++ src/Component/ContactMain/ContactMain.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import { FiMap } from "react-icons/fi";
-import { FiPhoneCall } from "react-icons/fi";
-import { MdEmail } from "react-icons/md";
 import './ContactMain.css'
 
 function ContactMain() {
@@ -17,36 +14,20 @@ function ContactMain() {
                 <hr />
                 <div className="bottom">
                     <div className="map">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7151.018966374255!2d84.19153286278645!3d26.342378457582694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3993ab7a9d94c765%3A0xb7d80c43302ddf3d!2sBaraipatti%20Durga%20temple!5e0!3m2!1sen!2sin!4v1684814219506!5m2!1sen!2sin" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7151.018966374255!2d84.19153286278645!3d26.342378457582694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3993ab7a9d94c765%3A0xb7d80c43302ddf3d!2sBaraipatti%20Durga%20temple!5e0!3m2!1sen!2sin!4v1684814219506!5m2!1sen!2sin" width="600" height="450" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                     <hr />
-                    <div className="box">
-                        <div className="card">
-                            <FiMap size='40px'
-                            />
-                            <h3>Address</h3>
-                            <p>Baraipatti, Kilpur, Nautan
-                                <br />
-                                Siwan, (Bihar) 841243.
-                            </p>
-                        </div>
-                        <div className="card">
-                            <FiPhoneCall size='40px'
-                            />
-                            <h3>Phone</h3>
-                            <p>Adarsh Kumar Singh
-                                <br />
-                                +91 7643043113
-                            </p>
-                        </div>
-                        <div className="card">
-                            <MdEmail size='40px'
-                            />
-                            <h3>Email</h3>
-                            <p>Adarsh Kumar Singh
-                                <br />
-                                adarshku5478953@gmail.com
-                            </p>
+                    <div className="form-heading">
+                    <h1>CONTACT FORM</h1>
+                    </div>
+                    <div className="container">
+                        <div className="contact-form">
+                            <form action='https://formspree.io/f/xpzeennp' method='POST' className='contact-inputs'>
+                                <input type='text' name='username' placeholder='UserName' autoComplete='off' required/>
+                                <input type='email' name='email' placeholder='Email' autoComplete='off' required/>
+                                <textarea name='message' cols="30" rows="6" placeholder='Message' autoComplete='off' required />
+                                <input className='btn' type='submit' value="Send"/>
+                            </form>
                         </div>
                     </div>
                 </div>
