5f9aee3f9b090590da6c8a1b0bae4a6873e430d1
Adarsh <adarshku7643@gmail.com>
2023-06-27 13:00:03 +0530

    Contact modified

--- src/Component/ContactMain/ContactMain.jsx
+++ src/Component/ContactMain/ContactMain.jsx
@@ -6,17 +6,13 @@ function ContactMain() {
         <>
             <div className="main">
                 <div className="top">
-                    <h1>CONTACT</h1>
+                    <h1>CONTACT ME</h1>
                     <div>
                         <img src="/contact.png" alt="contact" />
                     </div>
                 </div>
                 <hr />
                 <div className="bottom">
-                    <div className="map">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7151.018966374255!2d84.19153286278645!3d26.342378457582694!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3993ab7a9d94c765%3A0xb7d80c43302ddf3d!2sBaraipatti%20Durga%20temple!5e0!3m2!1sen!2sin!4v1684814219506!5m2!1sen!2sin" width="600" height="450" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" title='map'></iframe>
-                    </div>
-                    <hr />
                     <div className="form-heading">
                     <h1>CONTACT FORM</h1>
                     </div>
